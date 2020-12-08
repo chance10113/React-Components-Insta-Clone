c00e441b99d8afdd21443308e6824d0223640de3
Chance Swartz <chanceswartz@gmail.com>
2020-12-08 16:55:18 -0600

    Still struggling through geting Posts.js to work.

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData)
-
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
